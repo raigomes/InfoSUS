3b19dd54752df91126dae5c59240c588ed470573
raigomes <raigomes2@hotmail.com>
2014-10-19 20:52:45 -0200

    JQuery added and update frontend

--- InfoSUS App/InfoSUS App/www/js/index.js
+++ InfoSUS App/InfoSUS App/www/js/index.js
@@ -1,3 +1,39 @@
+/// <reference path="c:\users\t-raim\documents\visual studio 2012\projects\infosus\infosus app\infosus app\scripts\jquery-2.1.1.intellisense.js" />
+/// <reference path="c:\users\t-raim\documents\visual studio 2012\projects\infosus\infosus app\infosus app\scripts\jquery-2.1.1.js" />
+/// <reference path="c:\users\t-raim\documents\visual studio 2012\projects\infosus\infosus app\infosus app\scripts\jquery-2.1.1.min.js" />
 
-    
+document.addEventListener('deviceready', this.onDeviceReady, false);
+function onDeviceReady() {
+    $(document).ready(function () {                
+        $("#btn12").click(function () {//SUS
+            
+        });
+        $("#btn13").click(function () {//Telefones
+           
+        });
+        $("#btn14").click(function () {//About
+            
+        });
+        $("#btn21").click(function () {
 
+        });
+        $("#btn22").click(function () {
+
+        });
+        $("#btn23").click(function () {
+
+        });
+        $("#btn24").click(function () {
+
+        });
+        $("#btn25").click(function () {
+
+        });
+        $("#btn26").click(function () {
+
+        });
+        $("#btn27").click(function () {
+
+        });
+    });
+}
