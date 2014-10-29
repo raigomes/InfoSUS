7526e228e1f16aa50e15b58b28562c44000e3563
raigomes <raigomes2@hotmail.com>
2014-10-29 16:03:45 -0200

    Resolver leitura do arquivo JSON

--- InfoSUS App/InfoSUS App/www/js/index.js
+++ InfoSUS App/InfoSUS App/www/js/index.js
@@ -8,11 +8,52 @@ function onDeviceReady() {
         $("#btn12").click(function () {//SUS
             
         });
-        $("#btn13").click(function () {//Telefones
-           
-        });
-        $("#btn14").click(function () {//About
-            
+        $("#btn13").click(function () {//Telefones            
+            $("#phoneTable").find("tbody").empty().append(function () {
+                $.support.cors = true;
+                $.mobile.allowCrossDomainPages = true;
+                var path = window.location.pathname.replace('index.html', 'telefonesuteis.json');
+                alert(path);
+                
+                $.ajax(path, {
+                    type: "GET",
+                    dataType: "json",
+                    isLocal: true,
+                    success: function (data) {
+                        var answer = "";
+                        alert("entrou");
+                        console.log(data);
+                        data = $.parseJSON(data)
+                        $.each(data, function (key, val) {
+                            answer = "<td>" + val + "</td>";
+                        });
+                        alert(answer);
+                        return "<tr>" + answer + "</tr>";
+                    },
+                    error: function (msg) {
+                        alert("ERROR: " + msg.status + " " + msg.statusText);
+                    }
+                });
+                //$.getJSON('telefonesuteis.json', 
+                //    function (data) {
+                //        var answer = "";
+                //        alert("entrou");
+                //        $.each(data, function (key, val) {
+                //            answer = "<td>" + val + "</td>";
+                //        });
+                //        alert(answer);
+                //        return "<tr>" + answer + "</tr>";
+                //});
+                //var name = "<td>Servi&ccedil;o de Atendimento M&oacute;vel de Urg&ecirc;ncia</td>";
+                //var phone = "<td>192</td>";
+                //var description = "<td>Atendimento de Urg&ecirc;ncias M&eacute;dicas do SAMU</td>";
+                //var text = document.createElement("tr");
+                //text.innerHTML = name + phone + description;
+
+                //alert(text.innerHTML);
+
+                //return text;
+            });
         });
         $("#btn21").click(function () {
 
